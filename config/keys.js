eea7e59ddfa2c3b6e8c0e1e53215754e7338fae2
Bushra Bonkiye <bushrabonkiye@gmail.com>
2021-08-24 15:39:10 -0400

    Recommiting after saving changes not done for previous commit

--- config/keys.js
+++ config/keys.js
@@ -1,1 +1,8 @@
 //keys.js - figure out what set of credentials to return
+
+if(process.env.NODE_ENV === 'production') {
+    module.exports = require('./prod');
+} else {
+    //In development
+    module.exports = require('./dev');
+}
